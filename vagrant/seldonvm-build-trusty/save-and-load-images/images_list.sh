3d4e77e8e400c63249ddb821b7f7bd3d527bf0e0
Gurminder Sunner <gsunner2000@gmail.com>
2015-04-27 15:38:25 +0100

    images list changed to full one

--- vagrant/seldonvm-build-trusty/save-and-load-images/images_list.sh
+++ vagrant/seldonvm-build-trusty/save-and-load-images/images_list.sh
@@ -1,4 +1,16 @@
 declare -a IMAGES_LIST=(
-    "busybox" \
+    "api_server_demo_image" \
+    "api_server_data_logs_image" \
+    "mysql_image" \
+    "mysql_data" \
+    "memcache_image" \
+    "td_agent_image" \
+    "zookeeper_image" \
+    "spark_image" \
+    "seldon-models" \
+    "seldon-tools" \
+    "consul_image" \
+    "semantic_vectors_image" \
+    "movielens_data_transform" \
 )
 
