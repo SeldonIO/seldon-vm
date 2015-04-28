cff42e3fd46d02d530e9dd9850719721883a42d9
Gurminder Sunner <gsunner2000@gmail.com>
2015-04-28 17:35:34 +0100

    mf job submit updated

--- images/spark_image/spark-jobs/matrix-factorization.sh
+++ images/spark_image/spark-jobs/matrix-factorization.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -o nounset
 set -o errexit
 
 WORK_DIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -13,86 +14,27 @@ fi
 
 CLIENT=$1
 
-JSON=`curl -s http://consul:8500/v1/kv/seldon/${CLIENT}/algs/matrix_factorization?raw`
-echo "${JSON}"
-ARGS=""
-if [[ -z "${JSON}" ]]; then
-    JSON="{}"
-fi
+DATA_FOLDER=/seldon-models
+START_DAY=1
+MEM="3g"
 
-START_DAY=`echo "${JSON}" | jq -r ".start_day // empty"`
-if [ "${START_DAY}" = 'yesterday' ]; then
-    START_DAY=$(($(perl -e 'use POSIX;print strftime "%s",localtime time-86400;')/86400))
-fi
-if [[ ! -z "${START_DAY}" ]]; then
-    ARGS="${ARGS} ${START_DAY}"
-else
-    START_DAY=1
-    ARGS="${ARGS} 1"
-fi
-NUM_DAYS=`echo "${JSON}" | jq -r ".num_days // empty"`
-if [[ ! -z "${NUM_DAYS}" ]]; then
-    ARGS="${ARGS} ${NUM_DAYS}"
-else
-    ARGS="${ARGS} 1"
-fi
-RANK=`echo "${JSON}" | jq -r ".rank //empty"`
-if [[ ! -z "${RANK}" ]]; then
-    ARGS="${ARGS} ${RANK}"
-else
-    ARGS="${ARGS} 30"
-fi
-LAMBDA=`echo "${JSON}" | jq -r ".lambda // empty"`
-if [[ ! -z "${LAMBDA}" ]]; then
-    ARGS="${ARGS} ${LAMBDA}"
-else
-    ARGS="${ARGS} 0.1"
-fi
-ALPHA=`echo "${JSON}" | jq -r ".alpha // empty"`
-if [[ ! -z "${ALPHA}" ]]; then
-    ARGS="${ARGS} ${ALPHA}"
-else
-    ARGS="${ARGS} 1"
-fi
-ITERATIONS=`echo "${JSON}" | jq -r ".iterations // empty"`
-if [[ ! -z "${ITERATIONS}" ]]; then
-    ARGS="${ARGS} ${ITERATIONS}"
-else
-    ARGS="${ARGS} 6"
-fi
-ARGS="${ARGS} zookeeper_server"
-DATA_FOLDER=`echo "${JSON}" | jq -r ".data_folder // empty"`
-if [[ ! -z "${DATA_FOLDER}" ]]; then
-    ARGS="${ARGS} ${DATA_FOLDER}/${CLIENT}/actions/ ${DATA_FOLDER}/${CLIENT}/matrix-factorization/"
-else
-    ARGS="${ARGS} local://seldon-models/${CLIENT}/actions/ local://seldon-models/${CLIENT}/matrix-factorization/"
-    DATA_FOLDER="/seldon-models"
-fi
-
-
-JSON=`curl -s http://consul:8500/v1/kv/seldon/spark?raw`
-echo "${JSON}"
-if [[ -z "${JSON}" ]]; then
-    JSON="{}"
-fi
-MEM=`echo "${JSON}" | jq -r ".executor_memory // empty"`
-if [[ -z "${MEM}" ]]; then
-    MEM="5g"
-fi
-echo "Running with executor-memory ${MEM}"
+rm -rf ${DATA_FOLDER}/${CLIENT}/matrix-factorization/${START_DAY}
 
 JAR_FILE=`ls /app`
 JAR_FILE=`basename ${JAR_FILE}`
-echo "jar = ${JAR_FILE}"
+JAR_FILE_PATH=/app/${JAR_FILE}
+SPARK_HOME=/opt/spark
 
-rm -rf ${DATA_FOLDER}/${CLIENT}/matrix-factorization/${START_DAY}
-/opt/spark/bin/spark-submit \
-        --class io.seldon.spark.mllib.MfModelCreation \
-        --master local \
-        --executor-memory ${MEM} \
-	--driver-memory ${MEM} \
-        /app/${JAR_FILE} \
-        ${CLIENT} ${ARGS} \
-        awskey assecret
+echo "jar = ${JAR_FILE_PATH}"
+echo "Running with executor-memory ${MEM}"
 
+${SPARK_HOME}/bin/spark-submit \
+    --class "io.seldon.spark.mllib.MfModelCreation" \
+    --master "local" \
+    --executor-memory ${MEM} \
+    --driver-memory ${MEM} \
+    ${JAR_FILE_PATH} \
+        --client ${CLIENT} \
+        --zookeeper zookeeper_server \
+        --startDay ${START_DAY}
 
