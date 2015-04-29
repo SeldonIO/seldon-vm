5453440854a9cd18d3ed51f11badd4efeb67695a
Gurminder Sunner <gsunner2000@gmail.com>
2015-04-29 14:05:26 +0100

    some refactoring

--- dist/movie_recommender_demo/create_models.sh
+++ dist/movie_recommender_demo/create_models.sh
@@ -9,7 +9,7 @@ CLIENT=movielens
 MODEL=matrix_factorization
 MODEL=item_similarity
 MODEL=semantic_vectors
-MODEL=word2vec
+#MODEL=word2vec
 
 set_zk_node() {
     local ZK_NODE_PATH="$1"
@@ -19,14 +19,15 @@ set_zk_node() {
 }
 
 do_matrix_factorization() {
-    docker run --rm -i -t --name seldon_tools --link zookeeper_server_container:zk seldon-tools /seldon-tools/scripts/zookeeper/zkcmd.py --zk-hosts zk --cmd set --cmd-args /all_clients/movielens/offline/matrix-factorization '{"activate":true,"alpha":1,"days":1,"inputPath":"/seldon-models","iterations":5,"lambda":0.1,"local":true,"outputPath":"/seldon-models","rank":30,"startDay":1}'
+    set_zk_node '/all_clients/movielens/offline/matrix-factorization' \
+        '{"activate":true,"alpha":1,"days":1,"inputPath":"/seldon-models","iterations":5,"lambda":0.1,"local":true,"outputPath":"/seldon-models","rank":30,"startDay":1}'
 
     docker exec -it spark_offline_server_container bash -c "/spark-jobs/matrix-factorization.sh ${CLIENT}"
 }
 
 do_item_similarity() {
-    docker run --rm -i -t --name seldon_tools --link zookeeper_server_container:zk seldon-tools /seldon-tools/scripts/zookeeper/zkcmd.py --zk-hosts zk --cmd set --cmd-args \
-        /all_clients/movielens/offline/similar-items '{"inputPath":"/seldon-models","outputPath":"/seldon-models","days":1,"sample":0.25,"limit":100,"dimsum_threshold":0.5}'
+    set_zk_node '/all_clients/movielens/offline/similar-items' \
+        '{"inputPath":"/seldon-models","outputPath":"/seldon-models","days":1,"sample":0.25,"limit":100,"dimsum_threshold":0.5}'
 
     docker exec -it spark_offline_server_container bash -c "/spark-jobs/item-similarity.sh ${CLIENT}"
 
@@ -37,8 +38,7 @@ do_semantic_vectors() {
     MYSQL_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' 'mysql_server_container')
     MYSQL_ROOT_PASSWORD=mypass
     JOB_CONFIG='{"inputPath":"/seldon-models","outputPath":"/seldon-models","startDay":1,"days":1,"activate":true,"itemType":1,"itemLimit":10000,"tagAttrs":"movielens_tags_full","jdbc":"jdbc:mysql://'${MYSQL_HOST}':3306/movielens?user=root&password='${MYSQL_ROOT_PASSWORD}'&characterEncoding=utf8"}'
-    docker run --rm -i -t --name seldon_tools --link zookeeper_server_container:zk seldon-tools /seldon-tools/scripts/zookeeper/zkcmd.py --zk-hosts zk --cmd set --cmd-args \
-    /all_clients/movielens/offline/semvec "${JOB_CONFIG}"
+    set_zk_node '/all_clients/movielens/offline/semvec' "${JOB_CONFIG}"
 
     docker run --rm -i -t \
     --volumes-from seldon-models \
