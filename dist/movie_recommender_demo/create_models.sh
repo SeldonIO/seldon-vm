47cd1d2371d618440f03c53a1d9afd84b4761fd1
Gurminder Sunner <gsunner2000@gmail.com>
2015-04-29 11:58:32 +0100

    model creation for  semantic_vectors updated

--- dist/movie_recommender_demo/create_models.sh
+++ dist/movie_recommender_demo/create_models.sh
@@ -8,6 +8,7 @@ STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 CLIENT=movielens
 MODEL=matrix_factorization
 MODEL=item_similarity
+MODEL=semantic_vectors
 
 do_matrix_factorization() {
     docker run --rm -i -t --name seldon_tools --link zookeeper_server_container:zk seldon-tools /seldon-tools/scripts/zookeeper/zkcmd.py --zk-hosts zk --cmd set --cmd-args /all_clients/movielens/offline/matrix-factorization '{"activate":true,"alpha":1,"days":1,"inputPath":"/seldon-models","iterations":5,"lambda":0.1,"local":true,"outputPath":"/seldon-models","rank":30,"startDay":1}'
@@ -24,12 +25,28 @@ do_item_similarity() {
     docker run --name="upload_item_similarity_model" -it --rm --volumes-from seldon-models --link mysql_server_container:mysql_server  --link consul:consul seldon-tools bash -c "/seldon-tools/scripts/models/item-similarity/create_sql_and_upload.sh ${CLIENT}"
 }
 
+do_semantic_vectors() {
+    MYSQL_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' 'mysql_server_container')
+    MYSQL_ROOT_PASSWORD=mypass
+    JOB_CONFIG='{"inputPath":"/seldon-models","outputPath":"/seldon-models","startDay":1,"days":1,"activate":true,"itemType":1,"itemLimit":10000,"tagAttrs":"movielens_tags_full","jdbc":"jdbc:mysql://'${MYSQL_HOST}':3306/movielens?user=root&password='${MYSQL_ROOT_PASSWORD}'&characterEncoding=utf8"}'
+    docker run --rm -i -t --name seldon_tools --link zookeeper_server_container:zk seldon-tools /seldon-tools/scripts/zookeeper/zkcmd.py --zk-hosts zk --cmd set --cmd-args \
+    /all_clients/movielens/offline/semvec "${JOB_CONFIG}"
+
+    docker run --rm -i -t \
+    --volumes-from seldon-models \
+    --link zookeeper_server_container:zk \
+    seldonio/semantic-vectors-for-seldon bash -c './semvec/semantic-vectors.py --client movielens --zookeeper zk:2181'
+}
+
 case $MODEL in
     matrix_factorization)
         do_matrix_factorization
         ;;
     item_similarity)
         do_item_similarity
+        ;;
+    semantic_vectors)
+        do_semantic_vectors
         ;;
     *)
         echo "ignoring unkown model[$MODEL]"
