9f68965e3a50b5364853363e9a16c286bba6eecb
Gurminder Sunner <gsunner2000@gmail.com>
2015-04-30 12:11:31 +0100

    fix to seldon server logging

--- images/api_server_demo_image/startup.sh
+++ images/api_server_demo_image/startup.sh
@@ -3,7 +3,8 @@
 set -o nounset
 set -o errexit
 
-mkdir -p /mnt/labs/api/logs
+mkdir -p /data-logs/seldon-server
+ln -snv /data-logs/seldon-server /apps/tomcat7/logs/
 
 /apps/api-server/add_js_embedly_prefix.sh
 
--- images/api_server_image/apps/api-server/startup.sh
+++ images/api_server_image/apps/api-server/startup.sh
@@ -5,7 +5,8 @@ set -o errexit
 
 STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-mkdir -p /mnt/labs/api/logs
+mkdir -p /data-logs/seldon-server
+ln -snv /data-logs/seldon-server /apps/tomcat7/logs/
 
 /apps/tomcat7/bin/catalina.sh run
 
