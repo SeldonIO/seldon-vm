fb03d70feb44f13ee54bf2d1ac5fcffc16206206
Gurminder Sunner <gsunner2000@gmail.com>
2015-05-08 13:16:36 +0100

    start containers first

--- dist/movie_recommender_demo/create_recommender_demo.sh
+++ dist/movie_recommender_demo/create_recommender_demo.sh
@@ -9,11 +9,11 @@ CLIENT=movielens
 MODELS=( "matrix_factorization" "semantic_vectors" "word2vec" "item_similarity")
 YOUR_DATA_DIR=${STARTUP_DIR}/../your_data
 
-${STARTUP_DIR}/download_and_create_data.sh $CLIENT
-
 echo "restarting all containers to get clean state..."
 ${STARTUP_DIR}/../stop-all
 ${STARTUP_DIR}/start-all-for-models
+
+${STARTUP_DIR}/download_and_create_data.sh $CLIENT
 
 for model in "${MODELS[@]}"
 do
